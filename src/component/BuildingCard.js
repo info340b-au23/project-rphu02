d13232f11b8a3d046ef8f4fe0583947371fc0e92
rphu02 <rphu@uw.edu>
2023-12-11 02:26:58 -0800

    adding party size to card

--- src/component/BuildingCard.js
+++ src/component/BuildingCard.js
@@ -13,34 +13,48 @@ export function BuildingCard(props) {
     liStars[i] = <span key={i} className="fa fa-star checked"></span>
   }
 
+  var liHumans = [];
+  for (let i = 0; i < props.building.maxpartySize; i++) {
+    liHumans.push(<span key={i} className='fas fa-user-alt'></span>);
+  }
+
 
   return (
     <><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
-    <div className="col col-md-6 col-xl-4 d-flex">
-      <div className="card mb-4">
-        <div className="card-body">
-          <div className="row">
-            <div className="ci col col-sm-auto col-xl-12">
-              <img className="pb-3" src={props.building.img} alt={props.building.name} />
-            </div>
-            <div className="col-sm">
-              <h2 className="card-title"><b>{props.building.name}</b> <span style={{color: "#484848"}}> ({props.building.location})</span></h2>
-              {liStars}
-              <div className="row">
-                <div className="b">
-                  <Link to={'/Map'} className="btn btn-dark">
-                    Map
-                  </Link>
-                  <Link to={`/BlogForum/${props.building.name}`} className="btn btn-dark">
-                    Learn More
-                  </Link>
+      <div className="col col-md-6 col-xl-4 d-flex">
+        <div className="card mb-4">
+          <div className="card-body">
+              <div className="ci col col-sm-auto col-xl-12">
+                <img className="pb-3" src={props.building.img} alt={props.building.name} />
+              </div>
+              <div className="col-sm">
+                <h2 className="card-title"><b>{props.building.name}</b> <span style={{ color: "#484848" }}> ({props.building.location})</span></h2>
+              </div>
+              <div className='col-sm'>
+                <div className="row">
+                  <div className="col-md-6">
+                    {liStars}
+                  </div>
+                  <div className="col-md-6">
+                    <div className='humans'>
+                      {liHumans}
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="b">
+                    <Link to={'/Map'} className="btn btn-dark">
+                      Map
+                    </Link>
+                    <Link to={`/BlogForum/${props.building.name}`} className="btn btn-dark">
+                      Learn More
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
     </>
   );
 }
--- src/component/BuildingCardTable.js
+++ src/component/BuildingCardTable.js
@@ -296,42 +296,12 @@ export function BuildingCardTable(props) {
             </div>
         </div>;
 
-    // const pretendadded =
-    //     <><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
-    //         <div className="col col-md-6 col-xl-4 d-flex">
-    //             <div className="card mb-4">
-    //                 <div className="card-body">
-    //                     <div className="row">
-    //                         <div className="ci col col-sm-auto col-xl-12">
-    //                             <img className="pb-3" src={imageLink} alt={"cs_building"} />
-    //                         </div>
-    //                         <div className="col-sm">
-    //                             <h2 className="card-title"><b>{spotName}</b> <span style={{ color: "#484848" }}> ({buildingName === "otherb" ? tbuildingName : buildingName})</span></h2>
-    //                             {liStars}
-    //                             <div className="row">
-    //                                 <div className="b">
-    //                                     <Link to={'/Map'} className="btn btn-dark">
-    //                                         Map
-    //                                     </Link>
-    //                                     <Link to={`/BlogForum/${props.building.name}`} className="btn btn-dark">
-    //                                         Learn More
-    //                                     </Link>
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </>
+    
     return (
         <main>
             <div className="card-box container">
                 <div className="row">
                     {liCards}
-                    {/* {addSubmit === true &&
-                        pretendadded
-                    } */}
                     {addCard}
                 </div>
             </div>
