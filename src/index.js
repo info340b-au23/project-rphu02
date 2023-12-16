99371dc0099bbb56c12549e3467da26c50f093a6
Tim Carlson <timca@arenasports.net>
2023-12-16 09:55:49 -0800

    new firebase stuff to workaround the full db issue

--- src/index.js
+++ src/index.js
@@ -6,23 +6,32 @@ import './style.css';
 import { ref, child, getDatabase } from 'firebase/database';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDNh9t--U0J_WeBxt6KHdh0XiqSmqggix8",
+//   authDomain: "uw-study-spots.firebaseapp.com",
+//   databaseURL: "https://uw-study-spots-default-rtdb.firebaseio.com",
+//   projectId: "uw-study-spots",
+//   storageBucket: "uw-study-spots.appspot.com",
+//   messagingSenderId: "487068300917",
+//   appId: "1:487068300917:web:eced77a54ce24d2dda2a3c"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDNh9t--U0J_WeBxt6KHdh0XiqSmqggix8",
-  authDomain: "uw-study-spots.firebaseapp.com",
-  databaseURL: "https://uw-study-spots-default-rtdb.firebaseio.com",
-  projectId: "uw-study-spots",
-  storageBucket: "uw-study-spots.appspot.com",
-  messagingSenderId: "487068300917",
-  appId: "1:487068300917:web:eced77a54ce24d2dda2a3c"
+  apiKey: "AIzaSyATXG0Wb-cLvAY5ab0DsYOzCxZE9ctG3IQ",
+  authDomain: "group4-studyspots.firebaseapp.com",
+  projectId: "group4-studyspots",
+  storageBucket: "group4-studyspots.appspot.com",
+  messagingSenderId: "6461489128",
+  appId: "1:6461489128:web:f1584dadb7f45048172e5d"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 
 // const db = getDatabase();
